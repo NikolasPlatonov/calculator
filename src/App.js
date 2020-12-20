67b0af22f3ee891797454cd5e8cb291b16729d4a
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-12-20 16:40:15 +0200

    UI for calculator added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_container">
+      <div></div>
+      <div className="container">
+        <div className="input_container">
+          <div className="input">789787 </div>
+        </div>
+
+        <div className="btn_container">
+          <button className="btn_lightgrey">AC</button>
+          <button className="btn_lightgrey">+/-</button>
+          <button className="btn_lightgrey">%</button>
+          <button className="btn_orange">/</button>
+          {/*//////////////*/}
+          <button className="btn_darkgrey">mc</button>
+          <button className="btn_darkgrey">mr</button>
+          <button className="btn_darkgrey">m-</button>
+          <button className="btn_orange">m+</button>
+          {/*//////////////*/}
+          <button className="btn_darkgrey">7</button>
+          <button className="btn_darkgrey">8</button>
+          <button className="btn_darkgrey">9</button>
+          <button className="btn_orange">*</button>
+          {/*//////////////*/}
+          <button className="btn_darkgrey">4</button>
+          <button className="btn_darkgrey">5</button>
+          <button className="btn_darkgrey">6</button>
+          <button className="btn_orange">-</button>
+          {/*//////////////*/}
+          <button className="btn_darkgrey">1</button>
+          <button className="btn_darkgrey">2</button>
+          <button className="btn_darkgrey">3</button>
+          <button className="btn_orange">+</button>
+          {/*//////////////*/}
+          <button className="btn_null">0</button>
+          <button className="btn_darkgrey">,</button>
+          <button className="btn_orange">=</button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
