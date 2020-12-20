c8c1e4b2e5f18397521a94bac80d820976cf5e63
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-12-20 21:36:25 +0200

    Circle btn added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../src/App.css';
 
 function App() {
   return (
@@ -11,29 +11,41 @@ function App() {
         </div>
 
         <div className="btn_container">
-          <button className="btn_lightgrey">AC</button>
-          <button className="btn_lightgrey">+/-</button>
-          <button className="btn_lightgrey">%</button>
-          <button className="btn_orange">/</button>
-          <button className="btn_darkgrey">mc</button>
-          <button className="btn_darkgrey">mr</button>
-          <button className="btn_darkgrey">m-</button>
-          <button className="btn_orange">m+</button>
-          <button className="btn_darkgrey">7</button>
-          <button className="btn_darkgrey">8</button>
-          <button className="btn_darkgrey">9</button>
-          <button className="btn_orange">*</button>
-          <button className="btn_darkgrey">4</button>
-          <button className="btn_darkgrey">5</button>
-          <button className="btn_darkgrey">6</button>
-          <button className="btn_orange">-</button>
-          <button className="btn_darkgrey">1</button>
-          <button className="btn_darkgrey">2</button>
-          <button className="btn_darkgrey">3</button>
-          <button className="btn_orange">+</button>
-          <button className="btn_null">0</button>
-          <button className="btn_darkgrey">,</button>
-          <button className="btn_orange">=</button>
+          <div className="btn_row">
+            <button className="btn_lightgrey">AC</button>
+            <button className="btn_lightgrey">+/-</button>
+            <button className="btn_lightgrey">%</button>
+            <button className="btn_orange">/</button>
+          </div>
+          <div className="btn_row">
+            <button className="btn_darkgrey">mc</button>
+            <button className="btn_darkgrey">mr</button>
+            <button className="btn_darkgrey">m-</button>
+            <button className="btn_orange">m+</button>
+          </div>
+          <div className="btn_row">
+            <button className="btn_darkgrey">7</button>
+            <button className="btn_darkgrey">8</button>
+            <button className="btn_darkgrey">9</button>
+            <button className="btn_orange">x</button>
+          </div>
+          <div className="btn_row">
+            <button className="btn_darkgrey">4</button>
+            <button className="btn_darkgrey">5</button>
+            <button className="btn_darkgrey">6</button>
+            <button className="btn_orange">-</button>
+          </div>
+          <div className="btn_row">
+            <button className="btn_darkgrey">1</button>
+            <button className="btn_darkgrey">2</button>
+            <button className="btn_darkgrey">3</button>
+            <button className="btn_orange">+</button>
+          </div>
+          <div className="btn_row">
+            <button className="btn_null">0</button>
+            <button className="btn_darkgrey">,</button>
+            <button className="btn_orange">=</button>
+          </div>
         </div>
       </div>
       <div></div>
